1836e5f6d8f19c74125caef7842ae7b9d3cbb2d9
Soutif Thomas <th.soutif@gmail.com>
2024-07-28 16:18:12 +0200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@
 $ErrorActionPreference = "Stop"
 
 # Compile the PowerShell script into an executable
-Invoke-ps2exe .\WarframeMarketTool.ps1 .\dist\WarframeMarketTool.exe
+Invoke-ps2exe WarframeMarketTool.ps1 dist\WarframeMarketTool.exe
 
 # Create the dist directory if it doesn't exist
 if (-not (Test-Path -Path ".\dist")) {
@@ -13,7 +13,7 @@ if (-not (Test-Path -Path ".\dist")) {
 Copy-Item -Path ".\Views" -Destination ".\dist\Views" -Recurse -Force
 
 # Copy the config.json file to the dist directory
-Copy-Item -Path ".\config.json" -Destination ".\dist\config.json" -Force
+Copy-Item -Path "config.json" -Destination ".\dist\config.json" -Force
 
 Write-Host "###################################################################################"
 Write-Host "Build process completed."
