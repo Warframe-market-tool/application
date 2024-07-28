2833056db4f82d9afbcae02ff299285a7ee33be0
Soutif Thomas <th.soutif@gmail.com>
2024-07-28 16:27:37 +0200

    Update and rename build.bat to build.ps1

---
+++ build.ps1
@@ -0,0 +1,21 @@
+# Ensure script stops on first error
+$ErrorActionPreference = "Stop"
+# Import the ps2exe module
+Import-Module ps2exe
+# Compile the PowerShell script into an executable
+Invoke-ps2exe WarframeMarketTool.ps1 dist\WarframeMarketTool.exe
+
+# Create the dist directory if it doesn't exist
+if (-not (Test-Path -Path ".\dist")) {
+    New-Item -ItemType Directory -Path ".\dist"
+}
+
+# Copy the Views folder to the dist directory
+Copy-Item -Path ".\Views" -Destination ".\dist\Views" -Recurse -Force
+
+# Copy the config.json file to the dist directory
+Copy-Item -Path "config.json" -Destination ".\dist\config.json" -Force
+
+Write-Host "###################################################################################"
+Write-Host "Build process completed."
+Write-Host "###################################################################################"
