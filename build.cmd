5c4d278d68012ff4342ce699f2ccf9eea4fa1cb4
Soutif Thomas <th.soutif@gmail.com>
2024-07-28 15:34:33 +0200

    the output is now dist directory
    instead of bin, more coherent

--- build.cmd
+++ build.cmd
@@ -3,15 +3,15 @@ REM Compile the PowerShell script into an executable
 powershell -Command "Invoke-ps2exe .\WarframeMarketTool.ps1 .\bin\WarframeMarketTool.exe"
 
 REM Create the bin directory if it doesn't exist
-if not exist ".\bin\" (
-    mkdir ".\bin"
+if not exist ".\dist\" (
+    mkdir ".\dist"
 )
 
 REM Copy the Views folder to the bin directory
-xcopy ".\Views" ".\bin\Views" /E /I /Y
+xcopy ".\Views" ".\dist\Views" /E /I /Y
 
 REM Copy the config.json file to the bin directory
-copy ".\config.json" ".\bin\config.json" /Y
+copy ".\config.json" ".\dist\config.json" /Y
 echo ###################################################################################
 echo Build process completed.
 echo ###################################################################################
